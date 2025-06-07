d3a243a9db86c21940429544e82e01442a50dfa1
JensGevaert <Jens.gevaert@ugent.be>
2025-06-07 10:54:52 +0200

    fixed the elements from not aligning properly

--- img2asci/script.js
+++ img2asci/script.js
@@ -4,9 +4,9 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 const img = new Image();
-img.src = ("resources/jens.png");
+img.src = ("resources/spider.png");
 
-const keymap = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,. ";
+const keymap = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,.      ";
 
 function map(value, in_min, in_max, out_min, out_max) {
   return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
