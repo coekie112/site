b054267bbcd6fd02c91323814f6434a10537336d
coekie112 <jens.gevaert@gmail.com>
2025-06-09 23:37:58 +0200

    made menu clean

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
+const menubox = document.getElementById("menubox")
 
 // toggles menu on click
 hamburger.addEventListener('click', () => {
-    menu.classList.toggle('show');
-    hamburger.classList.toggle('show');
+    menu.classList.toggle('open');
+    hamburger.classList.toggle('closed')
+    menubox.classList.toggle('open')
+    topline.classList.toggle('animation')
+    bottomline.classList.toggle('animation')
 });
